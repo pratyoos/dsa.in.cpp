7b6ceb456b8515202847e1060f1c9f46a78143e4
Pratyoos Panta <pratyoos.panta@outlook.com>
2024-04-21 08:13:00 +0545

    Minor Updates

---
+++ inheritance.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+class Company{
+    public:
+        string brand;
+        Company(string x){
+            brand = x;
+        }
+        void honk(){
+            cout << "Tuut, tuut!" << endl;
+        }
+};
+
+//bike inherits from company
+//this is a proper way to inherit from a class
+
+class Bike: public Company{
+    public:
+        string model;
+        Bike(string x, string y):Company(x){
+            model = y;
+        }
+};
+
+int main(){
+    Bike myBike1("BMW", "X5");
+    Bike myBike2("Ford", "Mustang");
+    Bike myBike3("Toyota", "Corolla");
+
+    myBike1.honk();
+
+    cout << myBike1.brand << " " << myBike1.model << endl;
+    cout << myBike2.brand << " " << myBike2.model << endl;
+    cout << myBike3.brand << " " << myBike3.model << endl;
+
+    return 0;
+}

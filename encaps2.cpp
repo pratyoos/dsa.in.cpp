3441d0750950966082b5323c836eb2425e348a99
Pratyoos Panta <pratyoos.panta@outlook.com>
2024-04-23 21:45:53 +0545

    Update encaps2.cpp

--- encaps2.cpp
+++ encaps2.cpp
@@ -18,15 +18,11 @@ int main(){
     rectangle r3(6,7);
     rectangle r4(8,9);
     rectangle r5(10,11);
-    rectangle r6(12,13);
-    rectangle r7(14,15);
 
     cout << r1.getArea() << endl;
     cout << r2.getArea() << endl;
     cout << r3.getArea() << endl;
     cout << r4.getArea() << endl;
     cout << r5.getArea() << endl;
-    cout << r6.getArea() << endl;
-    cout << r7.getArea() << endl;
     return 0;
 }
